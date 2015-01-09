c2518997eafb74ff8747eca04f1550e206cfffe2
Yuxiang Wang <yw5aj@virginia.edu>
2015-01-09 14:37:31 -0500

    Finally got F90 working.

--- compileumat.py
+++ compileumat.py
@@ -12,16 +12,26 @@ compile_fortran=['ifort',
                  '/include:%I']
 
 
-def compile_umat(codename_list):
+def combine_modules(codename_list, objname):
+    umatfname = objname.replace('.obj', '.f90')
+    with open(umatfname, 'w') as fwrite:
+        for codename in codename_list:
+            with open(codename, 'r') as fread:
+                fwrite.write(fread.read())
+                fwrite.write('\n\n')
+    return umatfname
+
+def compile_umat(umatfname):
     assert compile_fortran[0] == 'ifort', 'This code only works with IVF.'
     compile_args = copy.deepcopy(compile_fortran)
-    compile_args.extend(codename_list)
+    compile_args.append(umatfname)
     assert subprocess.call(compile_args) == 0, \
         'Compilation error. See the command line window for more details.'
     return
 
 def run_umat(codename_list, jobname, objname, wait=False):
-    compile_umat(codename_list)
+    umatfname = combine_modules(codename_list, objname)
+    compile_umat(umatfname)
     mdb.jobs[jobname].setValues(userSubroutine=objname)
     mdb.jobs[jobname].submit(consistencyChecking=OFF)
     if wait:
@@ -30,10 +40,7 @@ def run_umat(codename_list, jobname, objname, wait=False):
 
 
 if __name__ == '__main__':
-    # codename_list = ['umat.f90', 'umatutils.f90', 'numerichyper.f90', 'modpsi_nh.f90']
-    codename_list = ['NeoHookeanNumeric.f90']
+    codename_list = ['umatutils.f90', 'modpsi_nh.f90',  'numerichyper.f90', 'subnumerichyper.f90']
     objname = 'umat.obj'
     jobname = 'SingleElem'
     run_umat(codename_list, jobname, objname)
-
-
