b918bd5bc1f12899510ba46e154489dd4855a3fd
Yuxiang Wang <yw5aj@virginia.edu>
2015-03-10 01:42:05 -0400

    Now complete Ogden, not tested with border cases yet

--- testogden.f90
+++ testogden.f90
@@ -62,7 +62,7 @@ contains
         integer, intent(in) :: nterms
         real(dp), intent(in) :: f(3, 3), isoprops(:)
         real(dp), intent(out) :: sigma(3, 3), ccj(3, 3, 3, 3)
-        integer :: i, j, k, l, n, k1, k2
+        integer :: i, j, k, l, n, k1, k2, a, b, c
         real(dp) :: mu(nterms), alpha(nterms), b(3, 3), lam2(3), lambar(3),&
             lbpow(3, nterms), det, lam(3), beta(3), gamma(3, 3), m(3, 3, 3),&
             d(3), dprime(3), i1, i3, ib(3, 3, 3, 3), dmterm1, dmterm2, dmterm3,&
@@ -164,8 +164,9 @@ contains
         ! Put everything together, for three distinct cases
         sigma = 0
         ccc = 0
-        ! If all three principal stretches are different
-        if ((lam(1) - lam(2) > eps) .and. (lam(1) - lam(3) > eps)) then
+        if ((lam(1) - lam(2) >= eps) .and. (lam(1) - lam(3) >= eps) .and.&
+            (lam(2) - lam(3) >= eps)) then
+            ! If all three principal stretches are different
             ! Get stress first
             do i = 1, 3
                 do j = i, 3
@@ -199,6 +200,66 @@ contains
                         end do    
                     end do
                     ! Fill symmetric part, i, j interchange
+                    if (i /= j) then
+                        ccc(j, i, :, :) = ccc(i, j, :, :)
+                    end if
+                end do
+            end do
+        elseif (lam(1) - lam(2) < eps .and. lam(1) - lam(3) < eps) then
+            ! All three are the same
+            do i = 1, 3
+                do j = i, 3
+                    do k = 1, 3
+                        do l = k, 3
+                            ccc(i, j, k, l) = gamma(1, 1) * 1.5_dp / det * (&
+                                ii(i, j, k, l) - delta(i, j) *&
+                                delta(k, l) / 3)
+                            if (k /= l) then
+                                ccc(i, j, l, k) = ccc(i, j, k, l)
+                            end if
+                        end do
+                    end do
+                    if (i /= j) then
+                        ccc(j, i, :, :) = ccc(i, j, :, :)
+                    end if
+                end do
+            end do
+        else then
+            ! Two are the same
+            ! First simplify into one case
+            if (lam(1) - lam(2) < eps) then c = 3
+            elseif (lam(1) - lam(3) < eps) then c = 2
+            elseif (lam(2) - lam(3) < eps) then c = 1
+            end if
+            a = mod(c + 1, 3)
+            b = mod(c + 2, 3)
+            ! Plug in
+            do i = 1, 3
+                do j = i, 3
+                    sigma(i, j) = (beta(a) * delta(i, j) + (beta(c) - beta(a))&
+                        * m(i, j, c)) / det
+                    if (i /= j) then
+                        sigma(j, i) = sigma(i, j)
+                    end if
+                    do k = 1, 3
+                        do l = k, 3
+                            ccc(i, j, k, l) = (&
+                                -beta(a) * 2 * ii(i, j, k, l)&
+                                + (beta(c) - beta(a)) * 2 * dm(i, j, k, l, c)&
+                                + gamma(a, a) * (&
+                                    delta(i, j) - m(i, j, c)) * (&
+                                    delta(k, l) - m(k, l, c))&
+                                + gamma(c, c) * m(i, j, c) * m(k, l, c)&
+                                + gamma(a, c) * (&
+                                    m(i, j, c) * (delta(k, l) - m(k, l, c))&
+                                    + (delta(i, j) - m(i, j, c)) * m(k, l, c)&
+                                    / det))&
+                                / det
+                            if (k /= l) then
+                                ccc(i, j, l, k) = ccc(i, j, k, l)
+                            end if
+                        end do
+                    end do
                     if (i /= j) then
                         ccc(j, i, :, :) = ccc(i, j, :, :)
                     end if
